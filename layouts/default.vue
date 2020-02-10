3319b77b67251acc649979bd1e8822291f04f1c8
Cody Bontecou <bontecouc@gmail.com>
2020-02-10 14:33:09 -0600

    removed v-app-bar and title until better styling.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,9 @@
 <template>
   <v-app class="primary">
 
-    <v-app-bar
-      app
-      color="transparent"
-      flat
-    >
-      <v-spacer></v-spacer>
-      <v-toolbar-title class="display-2 font-mono font-weight-light">Voice Cards</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
     <v-content>
       <nuxt/>
     </v-content>
 
   </v-app>
 </template>
-
-<script>
-  import {mapGetters} from 'vuex'
-
-  export default {
-    computed: {
-      ...mapGetters([]),
-      drawer: {
-        get() {
-          return this.$store.state.drawer
-        },
-        set(boolean) {
-          this.$store.commit('setDrawer', boolean)
-        }
-      }
-    },
-  }
-</script>
